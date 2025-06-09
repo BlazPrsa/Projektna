59b8eee89f79cb553307b92390fd900e63f2ab83
BlazPrsa <blaz.prsa005@gmail.com>
2025-06-09 13:49:51 +0200

    tabela 172

--- app/src/main/java/com/blazp/projektna/data/FirefighterTables.kt
+++ app/src/main/java/com/blazp/projektna/data/FirefighterTables.kt
@@ -2,7 +2,23 @@ package com.blazp.projektna.data
 
 val nozzleFlowTable = mapOf(
     1 to 94,
-    // Tabela 172
+    2 to 135,
+    3 to 165,
+    4 to 190,
+    5 to 215,
+    6 to 235,
+    7 to 250,
+    8 to 270,
+    9 to 285,
+    10 to 300,
+    11 to 315,
+    12 to 330,
+    13 to 345,
+    14 to 355,
+    15 to 370,
+    16 to 380,
+
+
 )
 
 val BPressureLossTable: Map<Pair<Int, Int>, Double> = mapOf(
